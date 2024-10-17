497c9396ef210974612dfd4d3c29b1bf006f943e
Siddharth Nahar <siddharth.nahar04@gmail.com>
2024-10-17 13:51:23 -0600

    edit,delete APIs

--- app.js
+++ app.js
@@ -38,7 +38,6 @@ app.get('/about', (req, res) => {
 app.post(
     '/add',
     [
-        // Validation rules
         body('firstname').notEmpty().withMessage('First name is required').isAlpha().withMessage('First name must contain only letters'),
         body('lastname').notEmpty().withMessage('Last name is required').isAlpha().withMessage('Last name must contain only letters'),
         body('email').isEmail().withMessage('Please enter a valid email'),
@@ -65,13 +64,66 @@ app.post(
                 console.error("Database Error: ", err);
                 // Return a response in case of a database error
                 return res.status(500).send("An error occurred while processing your request.");
-            } else{
-                res.render('thankyou', { name: `${firstname} ${lastname}`, email });
+            } else {
+                res.render('thankyou', { name: `${firstname} ${lastname}`,email });
             }
         });
     }
 );
 
+
+app.get('/getAllContacts', (req, res) => {
+    const sql = 'select * from contact_details';
+
+    //execute the query
+    db.query(sql, (err, result, field) => {
+        if (err) throw err;
+        res.render('contacts-list', { allContacts: result });
+    });
+})
+
+app.get('/edit/:id', (req, res) => {
+    const sql = 'select * from contact_details where id=?';
+    db.query(sql, [req.params.id], (err, result) => {
+        res.render("contact", { errors: [], formData: result[0] });
+    });
+})
+
+app.post('/edit/:id', (req, res) => {
+    const { firstname, lastname, email, PhoneNumber, city, province, postalcode, feedbackmessage } = req.body;
+    const sql = 'UPDATE contact_details' +
+        ' SET firstname = ?, lastname=?, email = ?, PhoneNumber=?,city=?,province=?,postalcode=?,feedbackmessage=?' +
+        ' where id = ?';
+    db.query(sql, [firstname, lastname, email, PhoneNumber, city, province, postalcode, feedbackmessage, req.params.id], (err, result) => {
+        if (err) {
+            throw err
+        } else {
+            const sql = 'SELECT * FROM contact_details';
+
+            db.query(sql, (err, result) => {
+                if (err) throw err;
+                res.render('contacts-list', { allContacts: result });
+            });
+        }
+    });
+})
+
+app.post('/delete/:id', (req, res) => {
+    const contactId = req.params.id;
+    const sql = 'DELETE FROM contact_details WHERE id = ?';
+    db.query(sql, [contactId], (err, result) => {
+        if (err) {
+            throw err;
+        } else {
+            const sql = 'SELECT * FROM contact_details';
+
+            db.query(sql, (err, result) => {
+                if (err) throw err;
+                res.render('contacts-list', { allContacts: result });
+            });
+        }
+    });
+});
 
 //endpoint for error pages
 app.use((req, res) => {
